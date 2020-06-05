24ca50d5668c56fcf4595d950bb23f70bb99fdf3
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-05 00:32:17 -0600

    header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import icon from '../images/icon.png'
+
+const Header = () => {
+
+    return (
+        <header>
+            <img src={icon} />
+            <h1>Elisabeth Artistry</h1>
+        </header>
+    )
+}
+
+export default Header
