be88b74e971287388e477b9d6460ce2e0f6cea20
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-05 14:13:41 -0600

    galleryPhotos objects with image, category, and name properties

--- src/images/galleryPhotos.js
+++ src/images/galleryPhotos.js
@@ -1,26 +1,122 @@
 export const galleryPhotos = [
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336291/elisabethArtistry/20200513_183701_uu8lzt.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336288/elisabethArtistry/20200513_164827_j67kwz.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336286/elisabethArtistry/20200318_112707_rzs7bf.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336282/elisabethArtistry/20200513_164838_dfp18f.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336277/elisabethArtistry/20200430_231918_hlpdvn.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336274/elisabethArtistry/20200430_231858_ovtbxw.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336271/elisabethArtistry/20191224_001128_hw70pd.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336267/elisabethArtistry/20200112_004839_ytwfbn.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336262/elisabethArtistry/20200216_110206_uuauud.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336261/elisabethArtistry/20191223_235220_bqqogk.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336261/elisabethArtistry/20191224_002030_plgft4.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336255/elisabethArtistry/20200127_182941_mrznqa.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336253/elisabethArtistry/20191224_000431_f4or1i.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336249/elisabethArtistry/20191224_000951_msjlu9.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336247/elisabethArtistry/20191223_235517_k12gyh.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336245/elisabethArtistry/20191223_235907_iejljd.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336242/elisabethArtistry/20191223_235448_ujhg4k.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336239/elisabethArtistry/20191224_000640_ftao27.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336231/elisabethArtistry/20191223_235001_lavo8r.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336228/elisabethArtistry/20190908_010610_jax6in.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336227/elisabethArtistry/20190908_010747_z6jsdc.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336227/elisabethArtistry/20190801_165639_zlr7jh.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336224/elisabethArtistry/20190801_165721_dkrvpy.jpg",
-    "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336221/elisabethArtistry/20190616_182255_k46ovi.jpg"
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336291/elisabethArtistry/20200513_183701_uu8lzt.jpg",
+        category: "Cakes",
+        name: "Rainbow Sprinkles"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336288/elisabethArtistry/20200513_164827_j67kwz.jpg",
+        category: "Cakes",
+        name: "Rainbow Sprinkles"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336286/elisabethArtistry/20200318_112707_rzs7bf.jpg",
+        category: "Crochet Cuddlies",
+        name: "Small Sea Turtle"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336282/elisabethArtistry/20200513_164838_dfp18f.jpg",
+        category: "Cakes",
+        name: "Rainbow Sprinkles"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336277/elisabethArtistry/20200430_231918_hlpdvn.jpg",
+        category: "Crochet Cuddlies",
+        name: "Hedgehogs"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336274/elisabethArtistry/20200430_231858_ovtbxw.jpg",
+        category: "Crochet Cuddlies",
+        name: "Hedgehogs"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336271/elisabethArtistry/20191224_001128_hw70pd.jpg",
+        category: "Crochet Games",
+        name: "Travel Chess and Checkers"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336267/elisabethArtistry/20200112_004839_ytwfbn.jpg",
+        category: "Crochet Cuddlies",
+        name: "Turtles"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336262/elisabethArtistry/20200216_110206_uuauud.jpg",
+        category: "Crochet Cuddlies",
+        name: "Small Sea Turtle"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336261/elisabethArtistry/20191223_235220_bqqogk.jpg",
+        category: "Dice Bags",
+        name: "Bulbasaur Dice Bag"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336261/elisabethArtistry/20191224_002030_plgft4.jpg",
+        category: "Crochet",
+        name: "Games and Bags"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336255/elisabethArtistry/20200127_182941_mrznqa.jpg",
+        category: "Cakes",
+        name: "Turtlespider"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336253/elisabethArtistry/20191224_000431_f4or1i.jpg",
+        category: "Crochet Games",
+        name: "Travel Tic Tac Toe"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336249/elisabethArtistry/20191224_000951_msjlu9.jpg",
+        category: "Crochet Games",
+        name: "Travel Chess and Checkers"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336247/elisabethArtistry/20191223_235517_k12gyh.jpg",
+        category: "Dice Bags",
+        name: "Mermaid Tail Bag"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336245/elisabethArtistry/20191223_235907_iejljd.jpg",
+        category: "Dice Bags",
+        name: "Pokeball Bag"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336242/elisabethArtistry/20191223_235448_ujhg4k.jpg",
+        category: "Dice Bags",
+        name: "Mermaid Tail Bag"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336239/elisabethArtistry/20191224_000640_ftao27.jpg",
+        category: "Crochet Games",
+        name: "Travel Chess and Checkers"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336231/elisabethArtistry/20191223_235001_lavo8r.jpg",
+        category: "Dice Bags",
+        name: "Crochet Bags"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336228/elisabethArtistry/20190908_010610_jax6in.jpg",
+        category: "Cakes",
+        name: "Mickey Mouse Cake"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336227/elisabethArtistry/20190908_010747_z6jsdc.jpg",
+        category: "Cakes",
+        name: "Mickey Mouse Cake"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336227/elisabethArtistry/20190801_165639_zlr7jh.jpg",
+        category: "Cakes",
+        name: "League Minimap Cake"
+    },        
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336224/elisabethArtistry/20190801_165721_dkrvpy.jpg",
+        category: "Cakes",
+        name: "League Minimap Cake"
+    },
+    {
+        image: "https://res.cloudinary.com/dwxkvhdoj/image/upload/v1591336221/elisabethArtistry/20190616_182255_k46ovi.jpg",
+        category: "Gifts",
+        name: "Dad Rock's gift"
+    }
 ]
