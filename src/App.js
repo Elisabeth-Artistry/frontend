6c75c87b3c65868a25de6515fc5c578313459403
tsbarrett89 <tsbarrett89@gmail.com>
2020-06-07 01:08:39 -0600

    started stripe integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from 'react'
+import { Route } from 'react-router'
 
 import Header from './components/Header'
 import GalleryCarousel from './components/GalleryCarousel'
+import Payment from './components/Payment'
 
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <GalleryCarousel />
+
+      <Route exact path='/' component={GalleryCarousel} />
+      <Route path='/payment' component={Payment} />
     </div>
   );
 }
---
+++ src/components/Payment.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
+
+import CheckoutForm from './CheckoutForm'
+
+const Payment = () => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Payment
