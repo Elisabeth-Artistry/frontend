a8e55aa76e38334f1383e1cc6cf09bb1e583986f
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-08 14:41:20 -0600

    classname added for styling

--- src/components/patterns/PatternCard.js
+++ src/components/patterns/PatternCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PatternCard = ({ pattern }) => {
 
     return (
-        <div>
+        <div className="patternCard">
             <h3>{pattern.name}</h3>
             <img src={pattern.image_url} alt={pattern.name} />
             <p>{pattern.description}</p>
