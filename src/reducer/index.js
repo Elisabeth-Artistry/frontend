3fdb23771b8b6619311a7f61e774a7d1a9326ff1
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-09 00:56:36 -0600

    pattern reducer created

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import cartReducer from './cartReducer'
 import userReducer from './userReducer'
+import patternReducer from './patternReducer'
 
 export default combineReducers({
     cartReducer,
-    userReducer
+    userReducer,
+    patternReducer
 })
