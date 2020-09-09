d45013bc1491676fd26db1903583454c7015c860
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-08 21:33:50 -0600

    id, axios call, state set

--- src/components/patterns/PatternPage.js
+++ src/components/patterns/PatternPage.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
+import '../../scss/patternPage.scss'
 
 const PatternPage = () => {
     const { id } = useParams()
+    const [pattern, setPattern] = useState({})
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/api/patterns/${id}`)
+            .then(res => {
+                console.log(res.data)
+                setPattern(res.data[0])
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     return (
-        <div>
-            {id}
+        <div className="patternPage">
+            <h2>{pattern.name}</h2>
+            <img src={pattern.image_url} alt={pattern.name} />
+            <p>{pattern.description}</p>
+            <p>{pattern.price}</p>
         </div>
     )
 }
