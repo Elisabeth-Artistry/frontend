03117b316640219c9894768545644df9649ffcd3
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-09 00:21:34 -0600

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './scss/reset.scss'
 
 import App from './App'
+import reducer from './reducer'
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, document.getElementById('root'))
