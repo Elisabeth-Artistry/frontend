f92036186b18c6fa429417707c12910b9613174e
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-09 16:04:08 -0600

    switch created

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,6 +1,17 @@
+import {
+    ADD_TO_CART,
+    REMOVE_FROM_CART
+} from '../actions/cart'
+
 const initialState = []
 
 const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_TO_CART:
+            return [...state, action.payload]
+        case REMOVE_FROM_CART:
+            return state.filter(pattern => pattern.id !== action.payload.id)
+    }
     return state
 }
     
