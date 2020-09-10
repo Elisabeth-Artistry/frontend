2fcfb90cb34143aacc8364d01c66bb528b2b1abb
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-10 13:04:58 -0600

    navlinks for home, patterns, and gallery

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
     return (
         <nav>
-
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/patterns'>Patterns</NavLink>
+            <NavLink to='/gallery'>Gallery</NavLink>
         </nav>
     )
 }
