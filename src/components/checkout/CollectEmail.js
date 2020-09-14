e3a31e37a69c420c5d271cf084ae3d350fcef4ee
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-14 13:48:50 -0600

    collect email form

--- src/components/checkout/CollectEmail.js
+++ src/components/checkout/CollectEmail.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const CollectEmail = () => {
+    const { register, handleSubmit, errors } = useForm();
+
+    const setEmail = () => {
+
+    }
 
     return (
-        <div></div>
+        <form onSubmit={handleSubmit(setEmail)}>
+            <lable>Email
+                <input
+                    name='email'
+                    ref={register({ required: true })}
+                />
+                {errors.email && <p>We need your email to send you the pattern.</p>}
+            </lable>
+        </form> 
     )
 }
 
